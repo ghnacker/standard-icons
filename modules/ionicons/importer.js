a4d13be5e3fe45fe4fb30b8697a81174f7c13446
Satoshi Nakagawa <ghnacker@gmail.com>
2020-10-04 11:02:20 +0900

    import from unoptimized

--- modules/ionicons/importer.js
+++ modules/ionicons/importer.js
@@ -27,7 +27,7 @@ let options = {
 
 let paths = {
   package: path.join(options.source, 'package.json'),
-  svgs: path.join(options.source, 'dist',  'collection/components/icon/svg'),
+  svgs: path.join(options.source, 'dist',  'svg'),
   dest: __dirname,
   svgsDest: path.join(__dirname, 'icons')
 };
